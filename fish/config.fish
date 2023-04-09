317d91ee82139d3da27b0d11a39e03369906b95c
Aaron Housh <dispersias@gmail.com>
2023-04-09 15:15:15 -0700

    add fish and zellij

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+if set -q ZELLIJ
+else
+    zellij --layout compact
+end
+
+# Generated for envman. Do not edit.
+test -s "$HOME/.config/envman/load.fish"; and source "$HOME/.config/envman/load.fish"
+
+fish_add_path $HOME/.cargo/bin
+
