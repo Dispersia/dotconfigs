56f5856e046b1df9e1f8c8e8d8b0ef06c4d7abb5
Aaron Housh <dispersias@gmail.com>
2024-04-09 23:17:43 +0900

    updates

--- nvim/lua/plugins/languages/cpp.lua
+++ nvim/lua/plugins/languages/cpp.lua
@@ -3,7 +3,5 @@ local lsp_settings = require('plugins.languages.settings')
 
 nvim_lsp.clangd.setup {
   capabilities = lsp_settings.Capabilities,
-  on_attach = function(client, bufnr)
-    lsp_settings.on_attach(client, bufnr)
-  end,
+  on_attach = lsp_settings.on_attach,
 }
