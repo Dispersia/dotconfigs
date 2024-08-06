6b8db4a5c0e3c29f7118404a68cf271a8f48f736
Aaron Housh <dispersias@gmail.com>
2024-08-06 09:12:11 -0700

    add files

--- fish/config.fish
+++ fish/config.fish
@@ -8,3 +8,6 @@ set -g fish_greeting
 # Generated for envman. Do not edit.
 test -s "$HOME/.config/envman/load.fish"; and source "$HOME/.config/envman/load.fish"
 
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/dispe/programs/google-cloud-sdk/path.fish.inc' ]; . '/home/dispe/programs/google-cloud-sdk/path.fish.inc'; end
