f2d8bc18077a16da203f6bcfbee9091a0fb1fd98
Aaron Housh <dispersias@gmail.com>
2024-12-20 15:05:19 -0700

    only start tailwind lsp in tailwind projects

--- nvim/lua/plugins/languages/typescript.lua
+++ nvim/lua/plugins/languages/typescript.lua
@@ -15,5 +15,10 @@ nvim_lspconfig.ts_ls.setup({
 
 nvim_lspconfig.tailwindcss.setup({
   capabilities = lsp_settings.Capabilities,
-  on_attach = lsp_settings.on_attach
+  on_attach = lsp_settings.on_attach,
+  root_dir = function(fname)
+    local root_pattern = nvim_lspconfig.util.root_pattern("tailwind.config.cjs", "tailwind.config.js",
+      "postcss.config.js")
+    return root_pattern(fname)
+  end
 })
