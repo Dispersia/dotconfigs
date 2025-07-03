73377251558029f8c1eddf8f194d243a01e5ad72
Aaron Housh <dispersias@gmail.com>
2025-07-02 20:34:58 -0700

    yup

--- nvim/lua/plugins/languages.lua
+++ nvim/lua/plugins/languages.lua
@@ -3,13 +3,11 @@ require('plugins.languages.rust')
 require('plugins.languages.scala')
 require('plugins.languages.wgsl')
 
-local mason_lspconfig = require('mason-lspconfig')
 local lspconfig = require('lspconfig')
 local lsp_settings = require('plugins.languages.settings')
+local mason_lspconfig = require('mason-lspconfig')
 
-for _, _settings in pairs(mason_lspconfig.get_installed_servers()) do
-  lspconfig[_settings].setup({
-    capabilities = lsp_settings.Capabilities,
-    on_attach = lsp_settings.on_attach,
-  })
-end;
+vim.lsp.config("*", {
+  capabilities = lsp_settings.Capabilities,
+  on_attach = lsp_settings.on_attach,
+})
