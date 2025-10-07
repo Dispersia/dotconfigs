1d406cfe605f5b8699289afc591d075d233a79c4
Aaron Housh <dispersias@gmail.com>
2025-10-07 10:28:53 -0700

    fix to always use lsp to format

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,16 +1,16 @@
 vim.api.nvim_create_autocmd("UIEnter", {
-	once = true,
-	callback = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				javascript = { "biome" },
-				lua = { "stylua" },
-				python = { "isort", "black" },
-				rust = { "rustfmt" },
-        sbt = { "scalafmt" },
-        scala = { "scalafmt" },
-				typescript = { "biome" },
-			},
-		})
-	end,
+  once = true,
+  callback = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        javascript = { "biome" },
+        python = { "isort", "black" },
+        rust = { "rustfmt" },
+        typescript = { "biome" },
+      },
+      default_format_opts = {
+        lsp_format = "fallback"
+      }
+    })
+  end,
 })
