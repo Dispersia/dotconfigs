be9a51053fa8ad8afa2d01a3b2624a9e60814c51
Aaron Housh <dispersias@gmail.com>
2025-10-13 13:49:24 -0700

    update metals config

--- nvim/lsp/metals.lua
+++ nvim/lsp/metals.lua
@@ -1,6 +1,20 @@
+local opts = table.concat({
+  "-Xmx12G",
+  "-Xss4m",
+  "-XX:+UseG1GC",
+  "-XX:+UseStringDeduplication",
+  "-XX:+UseNUMA",
+  "-XX:+ParallelRefProcEnabled",
+  "-XX:+AlwaysPreTouch"
+}, " ")
+
 ---@type vim.lsp.Config
 return {
   cmd = { "metals" },
+  cmd_env = {
+    JAVA_TOOL_OPTIONS = opts,
+    COURSIER_JAVA_OPTS = opts
+  },
   filetypes = { "scala", "sbt", "java" },
-  root_markers = { "build.mill" }
+  root_markers = { "build.mill", "build.sbt", "build.sc", ".git" },
 }
